aaa0646310ba7d619cefed9da3d0c55c555d7a9c
Todorov,Stefan S.S <s.todorov@student.fontys.nl>
2017-05-15 11:47:42 +0200

    Added a library for the navigation
    Added 3 buttons.
    Their colors will not stay the same

--- app/src/main/java/eu/toolegit/stefan/map/MainActivity.java
+++ app/src/main/java/eu/toolegit/stefan/map/MainActivity.java
@@ -1,13 +1,68 @@
 package eu.toolegit.stefan.map;
 
+import android.graphics.Color;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem;
+import com.aurelhubert.ahbottomnavigation.notification.AHNotification;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AHBottomNavigation bottomNavigation = (AHBottomNavigation) findViewById(R.id.bottom_navigation);
+
+// Create items
+        AHBottomNavigationItem item1 = new AHBottomNavigationItem(R.string.tab_1, R.drawable.ic_location_on_black_24dp,R.color.colorPrimary);
+        AHBottomNavigationItem item2 = new AHBottomNavigationItem(R.string.tab_2, R.drawable.ic_person_black_24dp, R.color.colorPrimary);
+        AHBottomNavigationItem item3 = new AHBottomNavigationItem(R.string.tab_3, R.drawable.ic_settings_black_24dp, R.color.colorPrimary);
+
+// Add items
+        bottomNavigation.addItem(item1);
+        bottomNavigation.addItem(item2);
+        bottomNavigation.addItem(item3);
+
+// Set background color
+        bottomNavigation.setDefaultBackgroundColor(Color.parseColor("#D3D3D3"));
+
+
+// Change colors
+        bottomNavigation.setAccentColor(Color.parseColor("#F63D2B"));
+        bottomNavigation.setInactiveColor(Color.parseColor("#747474"));
+// Force to tint the drawable (useful for font with icon for example)
+        bottomNavigation.setForceTint(true);
+
+        bottomNavigation.setTitleState(AHBottomNavigation.TitleState.SHOW_WHEN_ACTIVE);
+
+        AHNotification notification = new AHNotification.Builder()
+                .setText("1")
+                .setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.color_notification_back))
+                .setTextColor(ContextCompat.getColor(MainActivity.this, R.color.color_notification_text))
+                .build();
+        bottomNavigation.setNotification(notification, 1);
+
+
+        // Set listeners
+        bottomNavigation.setOnTabSelectedListener(new AHBottomNavigation.OnTabSelectedListener() {
+            @Override
+            public boolean onTabSelected(int position, boolean wasSelected) {
+                switch (position){
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                }
+                return true;
+            }
+        });
     }
+
+
 }
